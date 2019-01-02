5990d3518adf48182d44935d8a82198a4df48320
Rajiv Shankar <rajiv.shankar@kohls.com>
2019-01-01 19:06:36 -0800

    Ottergram example added

---
+++ front-end-dev-book/ottergram/scripts/main.js
@@ -0,0 +1,68 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY_CODE = 27;
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailImage.setAttribute("src", imageUrl);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+    showDetails();
+    setDetailsFromThumb(thumb);
+  });
+}
+
+function hideDetails(){
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails(){
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addHideDetailKeyPressEvent(){
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if(event.keyCode === ESC_KEY_CODE){
+      hideDetails();
+    }
+  });
+}
+
+function initializeEvents() {
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  thumbnails.forEach(function(thumb) {
+    addThumbClickHandler(thumb);
+  });
+  addHideDetailKeyPressEvent();
+}
+
+initializeEvents();
